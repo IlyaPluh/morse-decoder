422892c76a491d05e4565921dbcbd3bbd5f31717
IlyaPluh <ilyaandriyanov@mail.ru>
2022-07-27 18:18:44 +0300

    done

--- src/index.js
+++ src/index.js
@@ -37,9 +37,42 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
+// split binary word to binary letters 
+const SplitIntoLetters = function (word) {
+    let result = new Array();
+    while (word.length >= 10) {
+      result.push(word.splice(0, 10));
+    }
+    return result;
+  }
+
+  // remove zero from begin of binaty letter
+  const RemoveZeroFromBegin = function (word) {
+    while (word.length > 0 && Number(word[0]) === 0) {
+      word.shift();
+    }
+  }
+  
+  // change numbers to dot or dash
+  const MorseFromNumbers = function (word) {
+    let result = new Array();
+    RemoveZeroFromBegin(word);
+  
+    while (word.length > 0) {
+      result.push(word.splice(0, 2).join("") === "10" ? "." : "-");
+      RemoveZeroFromBegin(word);
+    }
+    return result.join("");
+  }
+  
+  function decode(line) {
+    return line.split("**********")
+      .map((word) => SplitIntoLetters(Array.from(word)))                                // split string into words
+      .map((splited_word) => splited_word.map(char => MorseFromNumbers(char)))          // split words into characters (morse format)
+      .map((splited_word) => splited_word.map(char => MORSE_TABLE[char]))               // get letter from morse code
+      .map(chars => chars.join(""))                                                     // join letters to word
+      .join(" ");                                                                       // join words to string
+    }
 
 module.exports = {
     decode
